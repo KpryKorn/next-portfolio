879ad1f40ff521e846c0a39808ebdecb90b33f5d
KpryKorn <roffinisacha@gmail.com>
2023-11-09 16:50:54 +0100

    fix: Update h3 tag to h2 tag for semantic reasons

--- components/ShowLastPosts.tsx
+++ components/ShowLastPosts.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function ShowLastPosts() {
   return (
     <section className="py-6 md:py-12 flex flex-col gap-12 text-gray-light">
-      <h3>Latest Posts</h3>
+      <h2>Latest Posts</h2>
       <ul className="flex flex-col gap-6 group">
         <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
           <span className="md:w-32">Oct 17, 2023</span>
