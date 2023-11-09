b5a89e4e4e514d28b8d21a92d52b916fc48ca4ca
KpryKorn <roffinisacha@gmail.com>
2023-11-09 16:22:18 +0100

    feat: add component to showcase last 3 posts on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,28 @@
 import ExternalLink from "@/components/ExternalLink";
 import HeroImage from "@/components/HeroImage";
 import HeroTitle from "@/components/HeroTitle";
+import ShowLastPosts from "@/components/ShowLastPosts";
 
 export default function Home() {
   return (
-    <article className="my-6 md:my-12 flex flex-col gap-8">
-      <HeroTitle
-        title="Sacha Roffini"
-        subtitle="I write code and ride motorcycles."
-      />
-      <HeroImage />
-      <p className="max-w-lg">
-        Hi, I'm Sacha Roffini, a software engineer (in the making) who loves
-        building cool things with code. In addition to coding, I am also
-        passionate about tech, creative design and motorcycles.
-      </p>
-      <div className="flex items-center gap-8">
-        <ExternalLink href="mailto:sacha@utopiaz.net" text="Email me" />
-        <ExternalLink href="#" text="More ways to connect" />
-      </div>
-    </article>
+    <>
+      <article className="my-6 md:my-12 flex flex-col gap-8">
+        <HeroTitle
+          title="Sacha Roffini"
+          subtitle="I write code and ride motorcycles."
+        />
+        <HeroImage />
+        <p className="max-w-lg">
+          Hi, I'm Sacha Roffini, a software engineer (in the making) who loves
+          building cool things with code. In addition to coding, I am also
+          passionate about tech, creative design and motorcycles.
+        </p>
+        <div className="flex items-center gap-8 group">
+          <ExternalLink href="mailto:sacha@utopiaz.net" text="Email me" />
+          <ExternalLink href="#" text="More ways to connect" />
+        </div>
+      </article>
+      <ShowLastPosts />
+    </>
   );
 }
