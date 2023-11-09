1c17e93972f0a0aba5208c9fbf5d47f3b4edf89e
KpryKorn <roffinisacha@gmail.com>
2023-11-09 16:21:58 +0100

    feat: add component to showcase last 3 posts on home page

---
+++ components/ShowLastPosts.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default async function ShowLastPosts() {
+  return (
+    <section className="py-6 md:py-12 flex flex-col gap-12 text-gray-light">
+      <h3>Latest Posts</h3>
+      <ul className="flex flex-col gap-6 group">
+        <li className="flex items-center gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
+          <span className="md:w-32">Oct 17, 2023</span>
+          <Link href={"#"} className="text-black-light font-medium">
+            My last blog post
+          </Link>
+        </li>
+        <li className="flex items-center gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
+          <span className="md:w-32">Nov 19, 2023</span>
+          <Link href={"#"} className="text-black-light font-medium">
+            My second blog post
+          </Link>
+        </li>
+        <li className="flex items-center gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
+          <span className="md:w-32">Oct 31, 2023</span>
+          <Link href={"#"} className="text-black-light font-medium">
+            My first blog post
+          </Link>
+        </li>
+      </ul>
+      <Link
+        href={"/blog"}
+        className="underline-offset-4 underline hover:text-black"
+      >
+        See All
+      </Link>
+    </section>
+  );
+}
