623b879ec594084100552a23f4f7a391598643a4
KpryKorn <roffinisacha@gmail.com>
2023-11-09 15:19:19 +0100

    misc: removed lucide react and add svg instead

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Sun } from "lucide-react";
 import Link from "next/link";
 
 export default function Header() {
@@ -58,7 +57,28 @@ export default function Header() {
           })}
         </ul>
         <span>
-          <Sun size={20} className="stroke-gray-light dark:stroke-gray-dark" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="stroke-gray-light dark:stroke-gray-dark"
+          >
+            <circle cx="12" cy="12" r="4" />
+            <path d="M12 3v1" />
+            <path d="M12 20v1" />
+            <path d="M3 12h1" />
+            <path d="M20 12h1" />
+            <path d="m18.364 5.636-.707.707" />
+            <path d="m6.343 17.657-.707.707" />
+            <path d="m5.636 5.636.707.707" />
+            <path d="m17.657 17.657.707.707" />
+          </svg>
         </span>
       </nav>
     </header>
