61ae175f76120ea660fb423168446ea9befb0881
KpryKorn <roffinisacha@gmail.com>
2023-11-09 10:56:46 +0100

    style: Add custom colors to tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "bg-light": "#FCFCFC",
+        "black-light": "#202020",
+        "gray-light": "#646464",
+        "bg-dark": "#0A0A0A",
+        "white-dark": "#FFFFFF",
+        "gray-dark": "#9CA3AF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
