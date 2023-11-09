2e63b2ddef1b9d789a99a8793b736726fa621598
KpryKorn <roffinisacha@gmail.com>
2023-11-09 13:44:20 +0100

    style: add antialiasing to fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body
         className={
           inter.className +
-          " container max-w-3xl bg-bg-light text-black-light dark:bg-bg-dark dark:text-white-dark"
+          " container antialiased max-w-3xl bg-bg-light text-black-light dark:bg-bg-dark dark:text-white-dark"
         }
       >
         <Header />
