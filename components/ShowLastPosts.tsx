1ca2734e9f0a34a767bb2f3dd78a23523d269f2e
KpryKorn <roffinisacha@gmail.com>
2023-11-12 10:22:44 +0100

    style: add creative hover img effect

--- components/ShowLastPosts.tsx
+++ components/ShowLastPosts.tsx
@@ -1,24 +1,83 @@
-import Link from "next/link";
+"use client";
 
-export default async function ShowLastPosts() {
+import Image from "next/image";
+import Link from "next/link";
+import imgArticle1 from "@/assets/unsplash-degrade-1.jpg";
+import imgArticle2 from "@/assets/unsplash-degrade-2.jpg.jpg";
+import imgArticle3 from "@/assets/unsplash-degrade-3.jpg.jpg";
+import { useState, useEffect } from "react";
+
+export default function ShowLastPosts() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  const imageStyle = {
+    top: mousePosition.y - 200,
+    left: mousePosition.x - 150,
+  };
+
   return (
     <section className="py-6 md:py-12 flex flex-col gap-12 text-gray-light">
       <h2>Latest Posts</h2>
-      <ul className="flex flex-col gap-6 group">
-        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
+      <ul className="flex flex-col gap-6 group/container">
+        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover/container:opacity-60 hover:!opacity-100 transition-opacity group/item">
+          <div
+            className="max-w-[300px] hidden absolute group-hover/item:block z-10"
+            style={imageStyle}
+          >
+            <Image
+              loading="eager"
+              src={imgArticle1}
+              width={300}
+              alt="Couverture de l'article du blog"
+              className="rounded-lg object-cover object-center aspect-video"
+            />
+          </div>
           <span className="md:w-32">Oct 17, 2023</span>
           <Link href={"#"} className="text-black-light font-medium">
             My last blog post
           </Link>
         </li>
-        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
-          <span className="md:w-32">Nov 19, 2023</span>
+        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover/container:opacity-60 hover:!opacity-100 transition-opacity group/item">
+          <div
+            className="max-w-[300px] hidden absolute group-hover/item:block z-10"
+            style={imageStyle}
+          >
+            <Image
+              loading="eager"
+              src={imgArticle2}
+              width={300}
+              alt="Couverture de l'article du blog"
+              className="rounded-lg object-cover object-center aspect-video"
+            />
+          </div>
+          <span className="md:w-32">Jan 10, 2023</span>
           <Link href={"#"} className="text-black-light font-medium">
-            My second blog post
+            What is Tailwind CSS
           </Link>
         </li>
-        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
-          <span className="md:w-32">Oct 31, 2023</span>
+        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover/container:opacity-60 hover:!opacity-100 transition-opacity group/item">
+          <div
+            className="max-w-[300px] hidden absolute group-hover/item:block z-10"
+            style={imageStyle}
+          >
+            <Image
+              loading="eager"
+              src={imgArticle3}
+              width={300}
+              alt="Couverture de l'article du blog"
+              className="rounded-lg object-cover object-center aspect-video"
+            />
+          </div>
+          <span className="md:w-32">Oct 02, 2023</span>
           <Link href={"#"} className="text-black-light font-medium">
             My first blog post
           </Link>
