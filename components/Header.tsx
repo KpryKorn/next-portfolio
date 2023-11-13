206b4ea454b32a16f88de74a8377a589559684a4
KpryKorn <roffinisacha@gmail.com>
2023-11-13 11:35:34 +0100

    style: add sticky header on bigger screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
   ];
 
   return (
-    <header>
+    <header className="relative md:sticky top-0 z-20 bg-bg-light dark:bg-bg-dark">
       <nav className="py-6 flex items-center justify-between">
         <div>
           <Link href={"/"}>
