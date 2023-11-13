5307eb8cbca371db94d18b6a1847d65f2e7da9a9
KpryKorn <roffinisacha@gmail.com>
2023-11-13 16:33:31 +0100

    feat: created /about page components

---
+++ components/AboutConnectItem.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+interface AboutConnectProps {
+  icon: any;
+  title: string;
+  href: string;
+}
+
+export default function AboutConnectItem({
+  icon,
+  title,
+  href,
+}: AboutConnectProps) {
+  return (
+    <Link href={href} target="blank">
+      <div className="col-span-1 flex justify-between items-center border border-gray-300 rounded-lg p-4 group-hover:opacity-60 hover:!opacity-100 transition-all">
+        <div className="flex gap-4 items-center">
+          {icon}
+          {title}
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M7 7h10v10" />
+          <path d="M7 17 17 7" />
+        </svg>
+      </div>
+    </Link>
+  );
+}
---
+++ components/AboutWorkItem.tsx
@@ -0,0 +1,36 @@
+import Image, { StaticImageData } from "next/image";
+
+interface AboutWorkItemProps {
+  imgSrc: string | StaticImageData;
+  jobTitle: string;
+  companyName: string;
+  duration: string;
+}
+
+export default function AboutWorkItem({
+  imgSrc,
+  jobTitle,
+  companyName,
+  duration,
+}: AboutWorkItemProps) {
+  return (
+    <li>
+      <figure className="flex gap-4 w-full py-2">
+        <Image
+          src={imgSrc}
+          width={50}
+          height={50}
+          alt={jobTitle}
+          className="rounded-full object-cover object-center w-[50px] h-[50px]"
+        />
+        <figcaption className="flex flex-grow items-start justify-between">
+          <div className="flex flex-col">
+            <h3>{jobTitle}</h3>
+            <p className="text-gray-light">{companyName}</p>
+          </div>
+          <p className="text-gray-light">{duration}</p>
+        </figcaption>
+      </figure>
+    </li>
+  );
+}
