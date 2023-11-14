e64284c68e650cc06699bd7dc2bcc65968b19166
KpryKorn <roffinisacha@gmail.com>
2023-11-14 16:48:33 +0100

    feat: Add HeroTitle and ShowLastPosts components to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import { allPosts } from "contentlayer/generated";
+import HeroTitle from "@/components/HeroTitle";
+import ShowLastPosts from "@/components/ShowLastPosts";
 
 export default function Page() {
-  const posts = allPosts.sort((a, b) =>
-    a.publishedAt > b.publishedAt ? -1 : a.publishedAt < b.publishedAt ? 1 : 0
-  );
-
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
-    </div>
+    <>
+      <HeroTitle
+        title="Blog"
+        subtitle="Here are some of the articles I've written."
+      />
+      <ShowLastPosts />
+    </>
   );
 }
