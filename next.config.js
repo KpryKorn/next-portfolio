fe698f1ed45aa576acfd0bd6db625e2cd478a8e8
KpryKorn <roffinisacha@gmail.com>
2023-11-14 13:23:50 +0100

    fix: contentlayer spelling

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const { withContentLayer } = require("next-contentlayer");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withContentLayer(nextConfig);
+module.exports = withContentlayer(nextConfig);
