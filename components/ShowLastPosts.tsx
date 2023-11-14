e4cb01b94cadcc2e22d2070e45f8a90db3ed4a48
KpryKorn <roffinisacha@gmail.com>
2023-11-14 16:48:49 +0100

    pending: working on client component

--- components/ShowLastPosts.tsx
+++ components/ShowLastPosts.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+// TODO: read https://nextjs.org/docs/app/api-reference/functions/use-params
+// accéder aux params de l'URL pour sélectionner le single post qui correspond post.slug et params.slug
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { allPosts } from "@/.contentlayer/generated";
+
 import imgArticle1 from "@/assets/unsplash-degrade-1.jpg";
 import imgArticle2 from "@/assets/unsplash-degrade-2.jpg.jpg";
 import imgArticle3 from "@/assets/unsplash-degrade-3.jpg.jpg";
-import { useState, useEffect } from "react";
 
 export default function ShowLastPosts() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -23,6 +28,8 @@ export default function ShowLastPosts() {
     top: mousePosition.y - 200,
     left: mousePosition.x - 150,
   };
+
+  const post = allPosts.find;
 
   return (
     <section className="py-6 md:py-12 flex flex-col gap-12 text-gray-light">
@@ -45,44 +52,6 @@ export default function ShowLastPosts() {
           <span className="md:w-32">Oct 17, 2023</span>
           <Link href={"#"} className="text-black-light font-medium">
             My last blog post
-          </Link>
-        </li>
-        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover/container:opacity-60 hover:!opacity-100 transition-opacity group/item">
-          <div
-            className="max-w-[300px] absolute z-10 scale-0 group-hover/item:scale-100 origin-bottom transition-transform ease-[cubic-bezier(.25,.75,.5,1.25)]"
-            style={imageStyle}
-          >
-            <Image
-              loading="eager"
-              src={imgArticle2}
-              width={300}
-              height={169}
-              alt="Couverture de l'article du blog"
-              className="rounded-lg object-cover object-center aspect-video"
-            />
-          </div>
-          <span className="md:w-32">Jan 10, 2023</span>
-          <Link href={"#"} className="text-black-light font-medium">
-            What is Tailwind CSS
-          </Link>
-        </li>
-        <li className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-8 group-hover/container:opacity-60 hover:!opacity-100 transition-opacity group/item">
-          <div
-            className="max-w-[300px] absolute z-10 scale-0 group-hover/item:scale-100 origin-bottom transition-transform ease-[cubic-bezier(.25,.75,.5,1.25)]"
-            style={imageStyle}
-          >
-            <Image
-              loading="eager"
-              src={imgArticle3}
-              width={300}
-              height={169}
-              alt="Couverture de l'article du blog"
-              className="rounded-lg object-cover object-center aspect-video"
-            />
-          </div>
-          <span className="md:w-32">Oct 02, 2023</span>
-          <Link href={"#"} className="text-black-light font-medium">
-            My first blog post
           </Link>
         </li>
       </ul>
