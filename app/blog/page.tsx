2c79f06f5e3c91e2e8ec57ca5129628a5bd8bd33
KpryKorn <roffinisacha@gmail.com>
2023-11-14 16:18:21 +0100

    refactor: remove PostCard component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,32 +1,7 @@
 import Link from "next/link";
-import { allPosts, Post } from "contentlayer/generated";
+import { allPosts } from "contentlayer/generated";
 
-function PostCard(post: Post) {
-  return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={`/blog/${post.slug}`}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
-      </h2>
-      <time
-        dateTime={post.publishedAt}
-        className="mb-2 block text-xs text-gray-600"
-      >
-        {post.publishedAt}
-      </time>
-      <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
-    </div>
-  );
-}
-
-export default function Home() {
+export default function Page() {
   const posts = allPosts.sort((a, b) =>
     a.publishedAt > b.publishedAt ? -1 : a.publishedAt < b.publishedAt ? 1 : 0
   );
@@ -36,9 +11,6 @@ export default function Home() {
       <h1 className="mb-8 text-center text-2xl font-black">
         Next.js + Contentlayer Example
       </h1>
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
     </div>
   );
 }
