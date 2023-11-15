7ac7f7aaa9311826829bc3f6248e1a31ae8341d3
KpryKorn <roffinisacha@gmail.com>
2023-11-15 15:41:28 +0100

    feat: added projects and project page is now dynamic

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,13 @@
+import { allProjects } from "@/.contentlayer/generated";
 import HeroTitle from "@/components/HeroTitle";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Projects | Sacha Roffini",
+  description: "Here are some of the projects I've worked on.",
+};
 
 export default function Page() {
   return (
@@ -9,85 +16,34 @@ export default function Page() {
         title="Projects"
         subtitle="Here are some of the projects I've worked on."
       />
-      <div className="my-6 md:my-12 py-6 md:py-12 flex flex-col gap-12">
-        <article>
-          <Link href={"/"}>
-            <figure className="flex flex-col md:flex-row gap-4 md:gap-8">
-              <Image
-                src={"/chien.webp"}
-                width={300}
-                height={169}
-                alt="Couverture de l'article du blog"
-                className="rounded-lg w-full object-cover object-center aspect-video"
-              />
-              <figcaption className="flex flex-col gap-2">
-                <div className="flex gap-2 items-center">
-                  <h2 className="text-xl font-medium">My portfolio</h2>
-                  <span className="text-sm">•</span>
-                  <span className="text-gray-light">2023</span>
-                </div>
-                <p className="text-gray-light">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quaerat voluptate vero, fugit dolore debitis quae enim
-                  veritatis beatae deserunt ab. Alias commodi assumenda rerum
-                  reprehenderit eum a, deleniti quisquam eaque.
-                </p>
-              </figcaption>
-            </figure>
-          </Link>
-        </article>
-        <article>
-          <Link href={"/"}>
-            <figure className="flex flex-col md:flex-row gap-4 md:gap-8">
-              <Image
-                src={"/degrade-1.jpg"}
-                width={300}
-                height={169}
-                alt="Couverture de l'article du blog"
-                className="rounded-lg w-full object-cover object-center aspect-video"
-              />
-              <figcaption className="flex flex-col gap-2">
-                <div className="flex gap-2 items-center">
-                  <h2 className="text-xl font-medium">Project n°2</h2>
-                  <span className="text-sm">•</span>
-                  <span className="text-gray-light">2022</span>
-                </div>
-                <p className="text-gray-light">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quaerat voluptate vero, fugit dolore debitis quae enim
-                  veritatis beatae deserunt ab. Alias commodi assumenda rerum
-                  reprehenderit eum a, deleniti quisquam eaque.
-                </p>
-              </figcaption>
-            </figure>
-          </Link>
-        </article>
-        <article>
-          <Link href={"/"}>
-            <figure className="flex flex-col md:flex-row gap-4 md:gap-8">
-              <Image
-                src={"/degrade-3.jpg"}
-                width={300}
-                height={169}
-                alt="Couverture de l'article du blog"
-                className="rounded-lg w-full object-cover object-center aspect-video"
-              />
-              <figcaption className="flex flex-col gap-2">
-                <div className="flex gap-2 items-center">
-                  <h2 className="text-xl font-medium">Third project</h2>
-                  <span className="text-sm">•</span>
-                  <span className="text-gray-light">2021</span>
-                </div>
-                <p className="text-gray-light">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quaerat voluptate vero, fugit dolore debitis quae enim
-                  veritatis beatae deserunt ab. Alias commodi assumenda rerum
-                  reprehenderit eum a, deleniti quisquam eaque.
-                </p>
-              </figcaption>
-            </figure>
-          </Link>
-        </article>
+      <div className="my-6 md:my-12 py-6 md:py-12 flex flex-col-reverse gap-12">
+        {allProjects.map((project) => {
+          return (
+            <article key={project._id}>
+              <Link href={`/projects/${project.slug}`}>
+                <figure className="flex flex-col md:flex-row gap-4 md:gap-8">
+                  <Image
+                    src={project.image}
+                    width={500}
+                    height={400}
+                    alt={project.title}
+                    className="rounded-lg w-full md:w-2/5 object-cover object-center aspect-video"
+                  />
+                  <figcaption className="flex flex-col gap-2">
+                    <div className="flex gap-2 items-center">
+                      <h2 className="text-xl font-medium">{project.title}</h2>
+                      <span className="text-sm">•</span>
+                      <span className="text-gray-light">
+                        {project.publishedAt}
+                      </span>
+                    </div>
+                    <p className="text-gray-light">{project.summary}</p>
+                  </figcaption>
+                </figure>
+              </Link>
+            </article>
+          );
+        })}
       </div>
     </>
   );
