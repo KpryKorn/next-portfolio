a6b44490e889d78a4f44194ad8d36823f266053f
KpryKorn <roffinisacha@gmail.com>
2023-11-16 10:52:14 +0100

    Add image component to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { allPosts } from "contentlayer/generated";
 import HeroTitle from "@/components/HeroTitle";
-import ShowLastPosts from "@/components/ShowLastPosts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -10,7 +10,45 @@ export default function Page() {
         title="Blog"
         subtitle="Here are some of the articles I've written."
       />
-      <ShowLastPosts />
+      <div
+        className="my-6 md:my-12 py-6 md:py-12 flex flex-col gap-12 animate-in"
+        style={{ "--index": 1 } as React.CSSProperties}
+      >
+        {allPosts
+          .sort(
+            (a, b) =>
+              new Date(b.publishedAt).getTime() -
+              new Date(a.publishedAt).getTime()
+          )
+          .filter((_, i) => i < 3)
+          .map((post) => {
+            return (
+              <article key={post._id}>
+                <Link href={`/blog/${post.slug}`}>
+                  <figure className="flex flex-col md:flex-row gap-4 md:gap-8">
+                    <Image
+                      src={post.image}
+                      width={500}
+                      height={400}
+                      alt={post.title}
+                      className="rounded-lg w-full md:w-2/5 object-cover object-center aspect-video"
+                    />
+                    <figcaption className="flex flex-col gap-2">
+                      <div className="flex gap-2 items-center">
+                        <h2 className="text-xl font-medium">{post.title}</h2>
+                        <span className="text-sm">•</span>
+                        <span className="text-gray-light">
+                          {post.publishedAt}
+                        </span>
+                      </div>
+                      <p className="text-gray-light">{post.summary}</p>
+                    </figcaption>
+                  </figure>
+                </Link>
+              </article>
+            );
+          })}
+      </div>
     </>
   );
 }
